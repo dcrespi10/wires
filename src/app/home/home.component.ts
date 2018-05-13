e5b30ca801b03e9ef001ff8dc5e73b009b2b5805
Daniele <danielecrespidev@gmail.com>
2018-05-13 18:19:59 +0200

    reading news from service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
-
 import { User, News } from '../_models/index';
-import { UserService } from '../_services/index';
+import { UserService, DataService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -10,19 +9,29 @@ import { UserService } from '../_services/index';
 
 export class HomeComponent implements OnInit {
     currentUser: User;
-    news: News[];
-    constructor(private userService: UserService) {
+    news: any;
+    constructor(private userService: UserService, private newsService: DataService,) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        this.news = [
-            {_id:"1d", title:"Welcome to the WIRES network!", text:"This is the WIRES news section. Here you can get any communication from the WIRES team, like news or follow-up to be made. Navigate throught the left side bar. Enjoy!",
-            date:"2018-04-20", expiryDate:"", priority:"low"},
-            {_id:"2a", "title":"Older news test", "text":"This is an older news with higher priority. We should group them somehow.",
-            "date":"2018-04-10", "expiryDate":"", "priority":"high"}
-        ];
+        this.newsService.setCollectionName("news");
+        this.news = [];
+        
     }
 
     ngOnInit() {
-        // shall load news
+        this.getNews();
     }
+
+    getNews(){
+        this.newsService.getCollection(this.currentUser._id, "news")
+        .subscribe(
+            data => {
+                console.log(data);
+                this.news = data;                            
+            },
+            error => {});
+    }
+}
+
+    
     
 }
